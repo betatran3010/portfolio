c8dd62a6e1d1c873e922e4dfb0fd5d5c325c5619
betatran3010 <161413260+betatran3010@users.noreply.github.com>
2026-01-14 07:26:00 +0000

    finished-portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,19 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+  loadEnv(mode, '.', '');
+
   return {
+    base: '/portfolio/',
+    plugins: [react()],
     server: {
       port: 3000,
       host: '0.0.0.0',
     },
-    plugins: [react()],
-    define: {},
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
-      }
-    }
+      },
+    },
   };
 });
